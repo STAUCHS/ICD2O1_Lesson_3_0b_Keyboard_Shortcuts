43cb6bed87cea7ffead05e64da1b3d4746a37dcf
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-04 18:43:42 -0500

    Updated Program1.py

--- Program1.py
+++ Program1.py
@@ -1,1 +1,27 @@
+# STEP 1:
+# Use the shortcuts to copy, cut, and paste the code below
+print("Hello World!")
 
+# STEP 2:
+# After copying, cutting, and pasting, try using the undo and redo shortcuts
+
+# STEP 3: 
+# Use the shortcuts to select and skip letters and words
+print("Select this letter: a")
+print("Skip this word: hello")
+print("Select this whole sentence")
+
+# STEP 4:
+# Comment out the line of code below
+print("I want this to be a comment")
+
+# STEP 5:
+# Duplicate the line below
+print("This sentence appears twice")
+
+# STEP 6:
+# Move print("World") and print("Hello")
+print("World")
+print("Hello")
+
+# STEP 7: Save your file using CTRL + S or COMMAND + S
